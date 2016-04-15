145d75af4c382f98d86d066ca18e7034a1e824b7
Finn <finn@callpipe.com>
2016-04-14 22:17:38 -0700

    Fix up le xml

--- status-grandstream-app.php
+++ status-grandstream-app.php
@@ -11,10 +11,18 @@ $valet_info = new SimpleXMLElement($xml);
 
 $out = new XMLWriter();
 $out->openMemory();
+// <?xml version="1.0" encoding="UTF-8">
 $out->startDocument('1.0','UTF-8');
+
+// <xmlapp title="Call Status">
 $out->startElement("xmlapp");
 $out->writeAttribute('title', 'Call Status');
+
+// <view>
 $out->startElement('view');
+
+// <section>
+$out->startElement('section');
 
 label($domain['domain_name']);
 
@@ -28,16 +36,29 @@ foreach($valet_info as $lot) {
   }
 }
 
-$out->startElement('Softkey');
-$out->writeAttribute('action', 'QuitApp');
-$out->writeAttribute('label', 'Cancel');
+// </section>
 $out->endElement();
 
+// </view>
+$out->endElement();
+
+// <Softkeys>
+$out->startElement('Softkeys');
+
+// <Softkey action="QuitApp" label="Exit" />
+$out->writeAttribute('action', 'QuitApp');
+$out->writeAttribute('label', 'Exit');
+$out->endElement();
+
+// <Softkey action="UseURL" label="Refresh" commandArgs="..." />
 $out->startElement('Softkey');
 $out->writeAttribute('action', 'UseURL');
 $out->writeAttribute('label', 'Refresh');
 $out->writeAttribute('commandArgs', 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']."?token=".$_GET['token']);
+
+// </Softkeys>
 $out->endElement();
 
+// </xmlapp>
 $out->endElement();
 echo $out->outputMemory();
